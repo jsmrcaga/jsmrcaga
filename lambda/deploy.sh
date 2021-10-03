8325692533a5455ce698b0523022dbae12230157
Jo Colina <jo.colina@weezevent.com>
2021-10-03 12:52:17 +0200

    Lambda: add template & dark mode

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -1,2 +1,3 @@
+rm ./lambda.zip || echo "No zip to delete, skipping..."
 zip -r lambda.zip *
 aws lambda update-function-code --function-name github-readme --zip-file fileb://lambda.zip --region eu-west-3
