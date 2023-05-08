0313872899a12c2ede91bd85037b9014fd051369
Jo Colina <jo@jocolina.com>
2023-05-08 22:24:46 +0200

    Landing Page: add tf config - vercel project & domain config

---
+++ tf/landing.tf
@@ -0,0 +1,27 @@
+# Handle custom domain if needed
+resource "cloudflare_record" "landing_custom_domain_dns_record" {
+  # only set domain if needed
+  zone_id = var.cloudflare.zone_id
+  # Naive subdomain spliting to get only the xxx part
+  name = "@"
+  value = "76.76.21.21"
+  type = "A"
+  ttl = 3600
+  proxied = false
+}
+
+# Create vercel project
+resource "vercel_project" "landing" {
+  name = "jocolina"
+  framework = "nextjs"
+  
+  environment = []
+
+  git_repository = null
+}
+
+# Attach domain to project
+resource "vercel_project_domain" "landing_custom_domain" {
+  project_id = vercel_project.landing.id
+  domain = "jocolina.com"
+}
--- tf/terraform.tf
+++ tf/terraform.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    vercel = {
+      source = "vercel/vercel"
+      version = "~> 0.11.5"
+    }
+
     cloudflare = {
       source = "cloudflare/cloudflare"
       version = "~> 3.10.1"
@@ -11,4 +16,8 @@ provider "cloudflare" {
   api_key = var.cloudflare.api_key
   email = var.cloudflare.email
   account_id = var.cloudflare.account_id
+}
+
+provider "vercel" {
+  api_token = var.vercel.token
 }
