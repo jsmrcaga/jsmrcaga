ceddbb4b49749fb13ef4d9a48656678522ef73af
Jo Colina <jo@jocolina.com>
2024-11-29 20:14:26 +0100

    Alloy: remove old alloy project

--- alloy/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-	namespace = kubernetes_namespace_v1.alloy.metadata[0].name
-	# same as in config.alloy
-	otel_collector_port = 4318
-}
--- alloy/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource kubernetes_namespace_v1 alloy {
-  metadata {
-    name = "alloy"
-  }
-}
