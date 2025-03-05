4c13d73370cbe0a68b341316cf327d8213b2b49b
Jo Colina <jo@jocolina.com>
2025-03-05 20:11:35 +0100

    Wireguard: add wireguard deployment via wg-easy

--- k3s-cluster/networking/wireguard/variables.tf
+++ k3s-cluster/networking/wireguard/variables.tf
@@ -1,4 +1,17 @@
+variable vpn_url {
+	type = string
+}
+
 variable peers {
 	type = list(string)
 	description = "List of allowed peers"
+}
+
+variable vrrp_virtual_ip {
+	type = string
+}
+
+variable vrrp_router_id {
+	type = number
+	default = 53
 }
