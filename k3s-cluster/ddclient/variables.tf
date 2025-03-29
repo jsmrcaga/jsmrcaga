365ab1d8bf5381bb2a2f04f484090984b5b59d61
Jo Colina <jo@jocolina.com>
2025-03-29 17:15:10 +0100

    ddclient: use variable in module for hostnames

--- k3s-cluster/ddclient/variables.tf
+++ k3s-cluster/ddclient/variables.tf
@@ -2,3 +2,8 @@ variable cloudflare_password {
   type = string
   sensitive = true
 }
+
+variable hostnames {
+  type = list(string)
+  description = "List of hostnames for which to update DNS records"
+}
